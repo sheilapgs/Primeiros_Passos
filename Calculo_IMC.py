8a8527db78a228ad207a4d200c17cac9af8624f8
Sheila <sheilapgsilva@gmail.com>
2025-03-30 16:59:13 -0300

    adicionando mais classificações da tabela do imc

--- Calculo_IMC.py
+++ Calculo_IMC.py
@@ -1,8 +1,19 @@
-massa = float(input("Digite a massa(kg): "))
+massa = float(input("Digite o peso(kg): "))
 altura = float(input("Digite a altura (m): "))
 imc = massa/(altura**2)
 
-if 18.5<=imc<=24.9:
-    print("SIM, você está dentro da sua faixa de peso ideal.")
+
+if imc <17:
+    print("Muito abaixo do peso")
+elif 17<=imc<=18.5:
+    print("Abaixo do peso")
+elif 18.5<imc<=24.9:
+    print("Peso ideal")
+elif 25>=imc<=29.9:
+    print("Sobrepeso")
+elif 30<=imc<=34.9:
+    print("Obesidade grau 1")
+elif 35<=imc<=39.9:
+    print("Obesidade grau 2")
 else:
-    print("Você NÃO está dentro da sua faixa de peso ideal")
+    print("Obesidade mórbida")
